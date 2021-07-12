bdb3c003da157f2a39e5ea1b80a6787f47c0f295
XRicko <idims52@gmail.com>
2021-07-12 19:48:44 +0300

    Extract component for updating name. Add ability to update todo item's name

---
+++ app/src/ToDoList.BlazorClient/Components/EditNameForm.razor.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+using ToDoList.SharedClientLibrary.Models;
+
+namespace ToDoList.BlazorClient.Components
+{
+    public partial class EditNameForm<TItem> : ComponentBase where TItem : BaseModel
+    {
+        private string originalName;
+        private bool isInFocus;
+        private string activeClass;
+
+        [Parameter]
+        public TItem Item { get; set; }
+
+        [Parameter]
+        public EventCallback OnValidSubmit { get; set; }
+
+        protected override void OnInitialized()
+        {
+            isInFocus = false;
+            originalName = Item.Name;
+            activeClass = "";
+        }
+
+        private async Task Submit()
+        {
+            await OnValidSubmit.InvokeAsync();
+            originalName = Item.Name;
+
+            RemoveFocus();
+        }
+
+        private void Reset()
+        {
+            Item.Name = originalName;
+            RemoveFocus();
+        }
+
+        private void SetFocus()
+        {
+            isInFocus = true;
+            activeClass = "active";
+        }
+
+        private void RemoveFocus()
+        {
+            isInFocus = false;
+            activeClass = "";
+        }
+    }
+}
